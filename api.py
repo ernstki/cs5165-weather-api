abec8df4686bd231aed6470c4bac621115aaa7ae
Kevin Ernst <ernstki@mail.uc.edu>
2017-02-15 00:30:29 -0500

    Finished GET, POST, and DELETE endpoints
    - includes Postman (v1) config for testing endpoints

--- api.py
+++ api.py
@@ -1,16 +1,104 @@
 #!/usr/bin/env python
 import os
+import sys
 import click
-from flask import Flask, render_template
+import pandas as pd
+import numpy as np
+from flask import Flask, request, render_template
+from flask_restful import abort, reqparse, Resource, Api
 
 app = Flask(__name__)
+api = Api(app)  # Flask-Restful API object
+
+app.debug = True
 app.config.update(
-    DEBUG=True,
     TEMPLATES_AUTO_RELOAD=True,
-    EXPLAIN_TEMPLATE_LOADING=True,
     PROJECT_ROOT=os.path.abspath(os.path.dirname(__file__)),
+    ERROR_404_HELP=False,
+    #EXPLAIN_TEMPLATE_LOADING=True,
 )
 
+# ref: http://pandas.pydata.org/pandas-docs/stable/io.html#specifying-column-data-types
+weather = pd.read_csv('static/daily.csv', header=0,
+                      dtype={'DATE': np.str})
+
+# A request parser for the POST operation
+postparser = reqparse.RequestParser(trim=True)
+postparser.add_argument('DATE', type=str,
+                         help='Invalid date (expected: YYYYMMDD as string)')
+postparser.add_argument('TMIN', type=float,
+                         help='Invalid temperature (expected: floating point)')
+postparser.add_argument('TMAX', type=float,
+                        help='Invalid temperature (expected: floating point)')
+
+
+def check_date(date):
+    """
+    Check to see if the given date exists in the DataFrame. Abort if not.
+    """
+    if not date in weather[['DATE']].values:
+        abort(404, message="No data found for %s" % date)
+
+
+# props to https://stackoverflow.com/a/26961568
+def df_to_reponse(df):
+    """
+    Make an 'application/json' reponse for a Pandas dataframe
+    """
+    # if there's only one record, un-arrayify it:
+    if len(df) == 1:
+        resp = df.iloc[0].to_json()
+    else:
+        # "orient='record'" removes the index from each record
+        resp = df.to_json(orient='records')
+
+    return app.response_class(resp, mimetype='application/json', status=200)
+
+
+class Temps(Resource):
+    dtypes = {'DATE': str, 'TMAX': float, 'TMIN': float}
+
+    def get(self):
+        """
+        Return all dates for which weather data exists
+        """
+        return df_to_reponse(weather[['DATE']])
+
+    def post(self):
+        global weather
+        args = postparser.parse_args()
+        if args.DATE in weather[['DATE']].values:
+            abort(400, message='Data for %s already exists' % args.DATE)
+
+        s = pd.Series(request.get_json())
+        weather = weather.append(s, ignore_index=True)
+
+        return {'DATE': args.DATE}, 201
+
+
+class Temp(Resource):
+    def get(self, date):
+        """
+        Return temperature data for a single date
+        """
+        check_date(date)
+        return df_to_reponse(weather[weather.DATE == date])
+
+    def delete(self, date):
+        """
+        Delete the temperature data for a single date
+        """
+        check_date(date)
+        weather.drop(weather.index[weather.DATE == '20170207'], inplace=True)
+        return '', 204
+
+
+api.add_resource(Temps, '/historical/')
+api.add_resource(Temp, '/historical/<string:date>')
+
+# ========================================================================
+#                              r o u t e s
+# ========================================================================
 
 @app.route('/')
 def index():
